7933404809aa8dba0bb56b128215236164adc66a
Sayed Iftikhar Qasimi <iftikharqasimi342@gmail.com>
2026-07-25 11:43:55 +0500

    Implemented Linear Search Algorithm

--- Array/Linear_Search.cpp
+++ Array/Linear_Search.cpp
@@ -9,28 +9,22 @@ int LinSearch(int arr[], int n , int target)
         if(arr[i] == target)
             return i;
     }
-
     return -1;
 }
 
 
 int main()
 {
-
     int n = 6; 
     int arr[n] = {5,19,23,9,10,45};
     int target = 10;
 
     int result = LinSearch(arr, n , target);
 
-
     if(result!=-1)
         cout << "The Target found on index " << result << endl;
     else
         cout << "Target not found in arra" << endl;
-
-
-
 
     return 0;
 }
