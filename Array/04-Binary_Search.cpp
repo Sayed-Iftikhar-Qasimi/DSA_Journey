0778b2cd5f1a518ef92b3a0caf25031d816d6128
Sayed Iftikhar Qasimi <iftikharqasimi342@gmail.com>
2026-07-26 13:20:43 +0500

    Implemented Binary Search Algorithm

---
+++ Array/04-Binary_Search.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std; 
+
+int BinSearch(int arr[], int n , int target)
+{
+        // {1,2,3,4,5,6,7,8,9};   5
+    int start = 0 ; 
+    int end = n; 
+
+    while(start<=end)
+    {
+       int mid = (start+end) / 2;
+
+        if(arr[mid]==target)
+            return mid;
+        else if(arr[mid] > target)
+            end = mid-1;
+        else 
+            start = mid+1; 
+    }
+
+    return -1;
+
+}
+
+int main()
+{
+    int n = 9;
+    int arr[n] = {1,2,3,4,5,6,7,8,9};
+    int target = 9;
+
+
+    int result = BinSearch(arr, n , target);
+
+    if(result!=-1)
+        cout << "Target found on index "<<result <<  endl;
+    else
+        cout << "Target not found in array" << endl;
+
+    return 0;
+ }
