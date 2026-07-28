b853b471f1c621f0f62f1aac5cc7ee95fed5298f
Sayed Iftikhar Qasimi <iftikharqasimi342@gmail.com>
2026-07-28 10:21:12 +0500

    Add print all subarrays (brute force) solution

---
+++ Array/05-Print_All_Subarrays.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std; 
+
+void PrintSubarrays(int arr[], int n)
+{
+
+    for(int start=0; start<n; start++)
+    {
+        for(int end=start; end<n; end++){
+            
+            for(int i=start; i<=end; i++)
+            { 
+                cout << arr[i];
+            }
+            cout << ",";
+
+        }
+        cout << endl;
+
+    }
+
+}
+
+int main()
+{
+    int n = 5; 
+    int arr[n] = {1,2,3,4,5};
+
+    PrintSubarrays(arr, n);
+
+
+    return 0;
+}
