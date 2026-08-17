933c0223044049e266ddd073776976febfaf3cd2
Sayed Iftikhar Qasimi <iftikharqasimi342@gmail.com>
2026-08-17 10:15:34 +0500

    valid Anagram Solution1

---
+++ Charater_Arary/04-Valid_Anagram.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool check_valid_Anagram(char arr[],char word[], int size)
+{
+    sort(arr, arr+size);
+    sort(word, word+size);
+
+    for(int i=0; i<size; i++)
+    {
+        if(arr[i] != word[i])
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    char ch[] = "khan";
+    char word[] = "khna";
+
+    bool result = check_valid_Anagram(ch,word, 4);
+
+    if(result)
+        cout << "Valid Anagram" << endl;
+    else
+        cout << "Non Valid Anagram " << endl;
+    return 0;
+}
