3dafe9e91439829292b650a81b45899a19d16a96
Sayed Iftikhar Qasimi <iftikharqasimi342@gmail.com>
2026-08-19 12:13:28 +0500

    Two Sum Problem Two Pointer Approach Solution

---
+++ Vector/03-Two_Sum(2).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std; 
+
+pair<int, int> TwoSum(vector<int> &nums, int target)
+{
+   
+    int start = 0 ; 
+    int end = nums.size() - 1; 
+    
+    while(start<end)
+    {
+        int current_sum = nums[start] + nums[end];
+        if(current_sum == target)
+            return {start, end};
+
+        else if(current_sum > target)
+            end--;
+        else
+            start++;
+    }
+
+    return {-1,-1};
+}
+
+int main()
+{
+    vector <int> nums = {2,7,8,9,11,15,17  ,20,22,28,31};
+    int target = 19;
+
+    auto p = TwoSum(nums, target);
+
+    cout << "(" << p.first << ", " << p.second << ")" << endl;
+ 
+
+    return 0;
+}
+
+
